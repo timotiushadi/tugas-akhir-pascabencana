9829f32dfbf99da6b3e370d2b13451b591a6b2c4
Ius <timotiushadipratama@gmail.com>
2022-04-15 12:56:19 +0700

    Add Chart.JS and fix bug

--- src/scripts/views/main.js
+++ src/scripts/views/main.js
@@ -225,7 +225,23 @@ document.querySelector(".popup .close-button").addEventListener("click", functio
   document.querySelector(".popup").classList.remove("active");
 })
 
+const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
 
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+// prevent form submit
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
 
 export default main;
 /*
